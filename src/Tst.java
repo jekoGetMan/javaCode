03edebeb4bd89b67dbc2f1dd27f0b7afc5594b7a
jekoGetMan <sl1mvsshady@gmail.com>
2018-11-23 13:31:12 +0200

    tst

--- src/Tst.java
+++ src/Tst.java
@@ -1,7 +1,7 @@
 public class Tst {
     public static void main(String[] args) {
-        System.out.println("lof");
-        System.out.println("lof");
+            System.out.println("lof");
+            System.out.println("lof");
         System.out.println("bob");
         System.out.println("bob");
     }
